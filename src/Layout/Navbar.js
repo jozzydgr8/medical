77d7edbe64e6348473554bcab0f83d9d5726ca5e
joshua <jozzydgreat1@gmail.com>
2024-07-13 13:16:19 -0700

    added abbr to nav

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -36,13 +36,13 @@ export const Navbar = ()=>{
 
     return(
     <nav id="navBottom">
-    <NavLink to="/medical" onClick={()=> setActiveNav('#') } className={activeNav === '#' ? 'active' : ''}> <ion-icon className='nav-icons'  name="home-outline"></ion-icon></NavLink>
+    <NavLink to="/medical" onClick={()=> setActiveNav('#') } className={activeNav === '#' ? 'active' : ''}> <abbr title='Home'><ion-icon className='nav-icons'  name="home-outline"></ion-icon></abbr></NavLink>
 
-    <NavLink to="/medical/cart" onClick={()=> setActiveNav('#cart') } className={activeNav === '#cart' ? 'active' : ''}><ion-icon className='nav-icons' name="cart-outline"></ion-icon>{ count > 0 &&<div className='badge'>{count}</div>}</NavLink>
+    <NavLink to="/medical/cart" onClick={()=> setActiveNav('#cart') } className={activeNav === '#cart' ? 'active' : ''}><abbr title="cart"><ion-icon className='nav-icons'  name="cart-outline"></ion-icon></abbr>{ count > 0 &&<div className='badge'>{count}</div>}</NavLink>
 
-    <a href="/medical#search" onClick={()=> setActiveNav('#search') } className={activeNav === '#search' ? 'active' : ''}><ion-icon className='nav-icons' name="search-outline"></ion-icon></a>
+    <a href="/medical#search" onClick={()=> setActiveNav('#search') } className={activeNav === '#search' ? 'active' : ''}><abbr title="search"><ion-icon className='nav-icons' name="search-outline"></ion-icon></abbr></a>
 
-    <NavLink to="/medical/orders" onClick={()=> setActiveNav('#order') } className={activeNav === '#order' ? 'active' : ''}><ion-icon className='nav-icons' name="bus-outline"></ion-icon>{ order && order.length > 0 &&<div className='badge'>{order.length}</div>}</NavLink>
+    <NavLink to="/medical/orders" onClick={()=> setActiveNav('#order') } className={activeNav === '#order' ? 'active' : ''}><abbr title="orders"><ion-icon className='nav-icons'  name="bus-outline"></ion-icon></abbr>{ order && order.length > 0 &&<div className='badge'>{order.length}</div>}</NavLink>
     
     
     
